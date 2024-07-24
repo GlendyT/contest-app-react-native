0e07f739add72330b7ebf6a8bf0adea29bd2a552
GlendyT <glendytuyuc@gmail.com>
2024-07-24 16:00:58 -0600

    terminar el ejercicio del count2

--- src/presentation/screens/settings/SettingsScreen.tsx
+++ src/presentation/screens/settings/SettingsScreen.tsx
@@ -1,23 +1,35 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useEffect} from 'react';
 import {Pressable, Text, View} from 'react-native';
 import {styles} from '../../../config/app-theme';
-import {useStoreCounter} from '../../store/counter-store';
+import {useCounterStore} from '../../store/counter-store';
+import {useNavigation} from '@react-navigation/native';
 
 export const SettingsScreen = () => {
-  const incrementBy = useStoreCounter(state => state.incrementBy);
-  const count = useStoreCounter(state => state.count);
+  const incrementBy = useCounterStore(state => state.incrementBy);
+  const count = useCounterStore(state => state.count);
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({
+      title: `Contador: ${count}`,
+    });
+  }, [count]);
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Counter: XXX</Text>
+      <Text style={styles.title}>Counter: {count}</Text>
 
       <Pressable
-        onPress={() => incrementBy(count)}
+        onPress={() => incrementBy(+1)}
         style={styles.primaryButton}>
         <Text style={styles.title}>+1</Text>
       </Pressable>
 
-      <Pressable style={styles.primaryButton}>
+      <Pressable
+        onPress={() => incrementBy(-1)}
+        style={styles.primaryButton}>
         <Text style={styles.title}>-1</Text>
       </Pressable>
     </View>
